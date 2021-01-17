1a862c27690afab6892b081e5acadc8ac34be602
lamaconda <kennethoffi6.6@gmail.com>
2021-01-17 08:27:21 +0300

    Task 1 is complete!

---
+++ task_1/src/main/java/by/lamaconda/even/IsEven.java
@@ -0,0 +1,15 @@
+package by.lamaconda.even;
+
+public class IsEven {
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+
+        /* if(number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+         */
+    }
+}
